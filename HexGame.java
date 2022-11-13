99f233997c19c9f5b7afeac86ca328af9eb4bc17
Soakeson <skyler.oakeson@gmail.com>
2022-11-12 17:02:59 -0700

    Added some comments.

--- HexGame.java
+++ HexGame.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 /**
  * HexGame takes a size and a filePath to intitalize the game state and play the game.
+ * It contains a play method which is the main gameloop.
  */
 public class HexGame {
     private HexState gameState;
@@ -19,10 +20,15 @@ public class HexGame {
         }
     }
 
+    /**
+     * Starts the main game loop of the hex and updates the game state everytime a move is executed.
+     * Reads each integer from the file and uses it as a cell index to take ownership of the indexed cell.
+     * Loops until either there is no more moves or a winner is decided. 
+     */
     public void play() {
         while (scanner.hasNextInt() && gameState.getWinner() == 0) {
             moves++;
-            gameState.update((moves % 2 == 0) ? ('R') : ('B'), scanner.nextInt());
+            gameState.update((moves % 2 == 0) ? ('R') : ('B'), scanner.nextInt()); // Swaps between players
         }
         if (gameState.getWinner() == -1)
             System.out.printf("\n--------> BLUE WON after %d moves", moves);
